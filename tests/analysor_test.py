d8f7b50e252611fe856ede45e4970761de204389
cafekrem <dutriezcle@gmail.com>
2023-01-07 20:23:21 +0100

    add draw 2D linear graph with seaborn

--- tests/analysor_test.py
+++ tests/analysor_test.py
@@ -3,12 +3,20 @@ import unittest
 from src.analysor import Analysor
 
 
+def fct_1(x):
+    return x + 1
+
+
+def fct_2( x):
+    return x * 2
+
 class AnalysorTest(unittest.TestCase):
     def test_instanciation(self):
         fct = lambda x: x**2
         input_dict = {fct : {"x" : range(0,10)}}
         analysor = Analysor(input_dict)
         self.assertDictEqual(analysor._functions,input_dict)
+        self.assertEqual(["x"],analysor._param_names)
 
     def test_no_argument_instanciation(self):
         analysor = Analysor()
@@ -31,6 +39,15 @@ class AnalysorTest(unittest.TestCase):
         self.assertListEqual([fct(x) for x in fct_dict["x"]],list(analysor._result[fct][analysor._target_name]))
         self.assertListEqual([fct2(x) for x in fct_dict["x"]],list(analysor._result[fct2][analysor._target_name]))
 
+
+    def test_draw_linear_2D_graph(self):
+        fct_dict = {"x": [1, 2, 3]}
+        analysor = Analysor()
+        analysor.add_function(fct_1, fct_dict)
+        analysor.add_function(fct_2, fct_dict)
+        # action
+        analysor.compute()
+        analysor.draw_linear_2D_graph()
 
 if __name__ == '__main__':
     unittest.main()
