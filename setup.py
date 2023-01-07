277df9fd4e5cd16494394b570d2b514529727472
cafekrem <dutriezcle@gmail.com>
2023-01-07 21:10:56 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='NumericalAnalysis',
+      version='0.1',
+      description='Numerical anlysis tool',
+      author='Clément Dutriez',
+      author_email='clement.dutriez@u-pem.fr',
+      url='https://github.com/CafeKrem/pyNumericalAnlysis',
+      packages=['pyAnalysis']
+     )
