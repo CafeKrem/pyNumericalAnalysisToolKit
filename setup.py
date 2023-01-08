003785a78a9668a214594fcd0428c5379c0717e3
Clement Dutriez <36420329+CafeKrem@users.noreply.github.com>
2023-01-08 11:31:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="numericalAnalysisTool",
-    version="0.1.3",
+    version="0.1.4",
     author="Clément Dutriez",
     author_email="clement.dutriez@u-pem.fr",
     description="Numerical anlysis tool kit",
