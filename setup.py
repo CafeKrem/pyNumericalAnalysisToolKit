e49fadcd34fbbf6590500c3d459720064704f405
Clement Dutriez <36420329+CafeKrem@users.noreply.github.com>
2023-01-08 11:27:10 +0100

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='numericalAnalysisTool',
-      version='0.1.2',
-      description='Numerical anlysis tool',
-      author='Clément Dutriez',
-      author_email='clement.dutriez@u-pem.fr',
-      url='https://github.com/CafeKrem/pyNumericalAnlysis',
-      packages=['numericalAnalysis'],
-      long_description=open('README.rst').read(),
-     )
+setuptools.setup(
+    name="numericalAnalysisTool",
+    version="0.1.3",
+    author="Clément Dutriez",
+    author_email="clement.dutriez@u-pem.fr",
+    description="Numerical anlysis tool kit",
+    url="https://github.com/CafeKrem/pyNumericalAnlysis",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'seaborn'
+    ]
+
+)
